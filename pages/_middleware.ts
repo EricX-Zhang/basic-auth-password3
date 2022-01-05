6156fa409518b69e9ebbf4834bd76d250ac906b6
EricZhang <972059096@qq.com>
2022-01-05 15:36:05 +0800

    添加日志并返回pos

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,6 +3,26 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
 
+  let env = process.env
+  let envStr = JSON.stringify(env)
+  let pos = req.geo
+  console.log('env', env)
+  console.log('pos', pos)
+
+
+  return new Response(
+    envStr, {
+    status: 200,
+    headers: {
+      'country': pos.country,
+      'city': pos.city,
+      'latitude': pos.latitude,
+      'longitude': pos.longitude,
+      'region': pos.region
+    }
+  }
+  )
+
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
